3ab4343dd177c6cea3503624b8ad011072277ea3
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-05-28 20:32:15 +1200

    finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,45 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import Jobs from "@/views/Jobs.vue";
-import NotFound from '@/views/NotFound.vue';
+import NotFound from "@/views/NotFound.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
+import EditJob from "@/views/EditJob.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'name',
-            component: HomeView
-        },
-        {
-            path: '/jobs',
-            name: 'Jobs',
-            component: Jobs
-        },
-        {
-            path: '/jobs/:id',
-            name: 'job',
-            component: JobView
-        },
-        {
-            path: '/:catchall(.*)',
-            name: 'Not Found',
-            component: NotFound
-        }
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "name",
+      component: HomeView,
+    },
+    {
+      path: "/jobs",
+      name: "Jobs",
+      component: Jobs,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
+    },
+    {
+      path: "/jobs/add",
+      name: "add-jop",
+      component: AddJobView,
+    },
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJob,
+    },
+    {
+      path: "/:catchall(.*)",
+      name: "Not Found",
+      component: NotFound,
+    },
+  ],
+});
 
 export default router;
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import BackButton from "@/components/BackButton.vue";
 import axios from "axios";
 import { reactive, onMounted } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute, RouterLink, useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
 
 const jobId = route.params.id;
 
@@ -13,25 +17,34 @@ const state = reactive({
   isLoading: true,
 });
 
-// onMounted(async function() {
-//     const reponse = await axios.get(`http://localhost:5000/jobs/${jobId}`);
-//     state.job = reponse.data;
-//     console.log(state.job)
-// })
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job");
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job Deleted Succesfully");
+      router.push("/jobs");
+    }
+  } catch (err) {
+    console.error("Error deleting job", err);
+    toast.error("Job Not Deleted");
+  }
+};
 
 onMounted(async function () {
   try {
-    const response = await axios.get(`http://localhost:5000/jobs/${jobId}`);
+    const response = await axios.get(`/api/jobs/${jobId}`);
     state.job = response.data;
-  }catch(err) {
-    console.error(`Error fetching job`, err)
-  }finally{
+  } catch (err) {
+    console.error(`Error fetching job`, err);
+  } finally {
     state.isLoading = false;
   }
 });
 </script>
 
 <template>
+  <BackButton></BackButton>
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -44,9 +57,7 @@ onMounted(async function () {
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
-              <i
-                class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-              ></i>
+              <i class="pi pi-map-marker text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -57,7 +68,7 @@ onMounted(async function () {
             </h3>
 
             <p class="mb-4">
-                {{ state.job.description }}
+              {{ state.job.description }}
             </p>
 
             <h3 class="text-green-800 text-lg font-bold mb-2">Salary</h3>
@@ -88,7 +99,9 @@ onMounted(async function () {
 
             <h3 class="text-xl">Contact Phone:</h3>
 
-            <p class="my-2 bg-green-100 p-2 font-bold">{{ state.job.company.contactPhone }}</p>
+            <p class="my-2 bg-green-100 p-2 font-bold">
+              {{ state.job.company.contactPhone }}
+            </p>
           </div>
 
           <!-- Manage -->
@@ -100,6 +113,7 @@ onMounted(async function () {
               >Edit Job</RouterLink
             >
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
